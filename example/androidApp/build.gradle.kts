073fd82b5e4022b42aaf15313fe82a7842f0fa8e
Alper Tekin <alpertekin6@gmail.com>
2023-11-30 12:49:22 +0300

    Upgrade Refiner SDK version to 1.3.1

--- example/androidApp/build.gradle.kts
+++ example/androidApp/build.gradle.kts
@@ -29,5 +29,5 @@ dependencies {
     implementation(AndroidX.constraintLayout)
     implementation(AndroidX.core.ktx)
     implementation(AndroidX.activity.ktx)
-    implementation("io.refiner:refiner:1.3.0")
+    implementation("io.refiner:refiner:1.3.1")
 }
